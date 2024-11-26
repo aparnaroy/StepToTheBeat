e94cd6973ea91e1678304f4199149105efec1a3d
aparnaroy <aparnar@udel.edu>
2024-11-26 16:35:38 -0500

    added gradient background and top menu bar

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # StepToTheBeat
 CISC 682 - Final Project
+
+
+### Attributions
+- Back Arrow Icon: <a href="https://www.flaticon.com/free-icons/arrow" title="arrow icons">Arrow icons created by Kirill Kazachek - Flaticon</a>
+- Home Icon: <a href="https://www.flaticon.com/free-icons/home" title="home icons">Home icons created by Freepik - Flaticon</a>
+- Settings Icon: <a href="https://www.flaticon.com/free-icons/settings" title="settings icons">Settings icons created by Pixel perfect - Flaticon</a>
--- app/src/main/java/com/example/steptothebeat/MainActivity.java
+++ app/src/main/java/com/example/steptothebeat/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.steptothebeat;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,5 +24,42 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Set up the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Enable the back button
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        switch (item.getItemId()) {
+//            case android.R.id.home:
+//                // Handle back button click
+//                onBackPressed();
+//                return true;
+//            case R.id.action_home:
+//                // Handle home button click
+//                Toast.makeText(this, "Home clicked", Toast.LENGTH_SHORT).show();
+//                return true;
+//            case R.id.action_settings:
+//                // Handle settings button click
+//                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show();
+//                return true;
+//            default:
+//                return super.onOptionsItemSelected(item);
+//        }
+//    }
 }
