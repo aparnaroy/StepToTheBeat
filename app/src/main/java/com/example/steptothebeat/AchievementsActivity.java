59ab19ae3b9df2902f85def2cfba5e405ec0d479
jkuchma <47617649+jkuchma@users.noreply.github.com>
2024-12-03 20:17:29 -0500

    Added menu at the top and created locked/unlocked functionality

--- app/src/main/java/com/example/steptothebeat/AchievementsActivity.java
+++ app/src/main/java/com/example/steptothebeat/AchievementsActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,8 +17,16 @@ public class AchievementsActivity extends BaseActivity{
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.achievements); //Set layout to achievements.xml page
+        setupToolbar(R.id.menubar,true);
+        addMenuBarSpace(R.id.achievements);
 
-        setupToolbar(R.id.menubar, true);
-        EdgeToEdge.enable(this);
+        boolean walkAchievementUnlocked = getSharedPreferences("achievements",MODE_PRIVATE).getBoolean("walk_achievement_unlocked",false);
+
+        if (walkAchievementUnlocked){
+            LinearLayout achievement1 = findViewById(R.id.achievement1);
+            TextView achievementText = (TextView) achievement1.getChildAt(0);
+            achievementText.setText("Walk with the app for the first time!\nStatus: Unlocked!");
+        }
     }
+
 }
