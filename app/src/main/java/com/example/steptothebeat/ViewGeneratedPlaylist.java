8264198a27315a3c8fccb746782017f0c1f3596a
Emilie <emiliea@udel.edu>
2024-12-03 19:29:33 -0500

    finishing select genre activity, skeleton view playlist page

--- app/src/main/java/com/example/steptothebeat/ViewGeneratedPlaylist.java
+++ app/src/main/java/com/example/steptothebeat/ViewGeneratedPlaylist.java
@@ -1,6 +1,8 @@
 package com.example.steptothebeat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class ViewGeneratedPlaylist extends BaseActivity {
@@ -13,10 +15,22 @@ public class ViewGeneratedPlaylist extends BaseActivity {
 
         String playlist = getIntent().getStringExtra("playlist");
 
+        // Buttons
+        LinearLayout startRun = findViewById(R.id.start_run);
+
+        // Set Listeners
+        startRun.setOnClickListener(view -> startRun());
+
         TextView selectedActivityTextView = findViewById(R.id.selected_playlist_text);
         if (playlist != null) {
-            selectedActivityTextView.setText("Selected: " + playlist);
+            selectedActivityTextView.setText(playlist);
         }
 
     }
+
+    public void startRun() {
+        Intent intent = new Intent(this, ActiveSessionActivity.class);
+        startActivity(intent);
+    }
+
 }
